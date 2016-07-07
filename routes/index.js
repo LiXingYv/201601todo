ade6b96d636af4fe15729a6f7c343d82ef54747b
LiXingYv <1450982558@qq.com>
2016-07-07 15:07:48 +0800

    完成

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/last', function(req, res, next) {
+    res.render('last', { title: 'Express' });
+});
+
 //给路由先指定路径，再指定方法，还可以链式调用
 router.route('/todos').get(function(req,res,next){
     todoModel.find({},function(err,todos){
@@ -27,6 +31,19 @@ router.route('/todos').get(function(req,res,next){
         }
     })
 });
+
+router.route('/todos/delete').post(function(req,res){
+    var ids = req.body;
+    console.log('123');
+    todoModel.remove({_id:{$in:ids}},function(err,result){
+        if(err){
+            res.send({code:0,msg:'删除失败！'});
+        }else{
+            //把保存到数据库中之后的对象发送给客户端
+            res.send({code:1,msg:'删除成功！'});
+        }
+    });
+});
 //删除操作
 router.route('/todos/:id').delete(function(req,res){
 
@@ -40,16 +57,5 @@ router.route('/todos/:id').delete(function(req,res){
     });
 })
 
-router.route('/todos/delete').delete(function(req,res){
-    var ids = req.body;
-    console.log('123');
-    todoModel.remove({_id:{$in:ids}},function(err,result){
-        if(err){
-            res.send({code:0,msg:'删除失败！'});
-        }else{
-            //把保存到数据库中之后的对象发送给客户端
-            res.send({code:1,msg:'删除成功！'});
-        }
-    });
-})
+
 module.exports = router;
